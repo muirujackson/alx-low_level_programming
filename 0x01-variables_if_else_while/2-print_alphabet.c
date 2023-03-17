5c39b492636044ddbe8ed5480eaa9c5ba6ae636d
muirujackson <jakezweru@gmail.com>
2023-03-17 16:47:08 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0, alpha = a;
+	for ( ;i<=26; )
+	{
+		alpha += i;
+		putchar('alpha');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
