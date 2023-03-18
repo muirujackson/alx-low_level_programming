bc946ece39845efdf5f9f8609810b6dd4feddf2e
muirujackson <jakezweru@gmail.com>
2023-03-17 17:41:28 -0700

    updated upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-        int i, j, alpha = 'a';
-        /* Print all lowercase letters */
-        for (i = 0; i < 26; i++)
-        {
-                putchar(alpha + i);
+	int i, j, alpha = 'a';
+	/* Print all lowercase letters */
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha + i);
         }
-        /* Print all uppercase letters*/
-        alpha = 'A';
-        for (j = 0; j < 26; j++)
-        {
-                putchar(alpha + j);
-        }
-        putchar('\n');
-
-        return 0;
+	/* Print all uppercase letters*/
+	alpha = 'A';
+	for (j = 0; j < 26; j++)
+	{
+		putchar(alpha + j);
+	}
+	putchar('\n');
+	
+	return 0;
 }
