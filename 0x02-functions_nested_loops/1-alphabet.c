2bf12aaec6cf4a256065b46c8d3213910bca3e05
muirujackson <jakezweru@gmail.com>
2023-03-21 11:15:54 -0700

    corrected alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - a to z
  *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+ * */
 
 void print_alphabet(void)
 {
