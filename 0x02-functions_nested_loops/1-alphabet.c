ccba537e64ad242dab58d69c992b99925e187c85
muirujackson <jakezweru@gmail.com>
2023-03-21 11:34:38 -0700

    betty style added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - a to z
+ * print_alphabet - utilizes on the _putchar function to print
  *
- * */
+*/
 
 void print_alphabet(void)
 {
 	int i, j = 'a';
 
-	for(i = 0; i < 26; i++)
-		_putchar(i+j);
+	for (i = 0; i < 26; i++)
+		_putchar( i +j);
 	_putchar('\n');
 
 }
