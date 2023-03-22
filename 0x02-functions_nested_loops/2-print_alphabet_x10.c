826fe9f646d52eba4f0eef73026e6eabbed65e97
muirujackson <jakezweru@gmail.com>
2023-03-22 08:53:44 -0700

    update betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 #include"main.h"
 
 /**
  * print_alphabet_x10 - function that will print the alphabet 10 times
- *
-*/
+ */
 
 void print_alphabet_x10(void)
 {
@@ -17,24 +15,3 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
-=======
-#include "main.h"
-
-/**
- * 
- * print alphabet a to z ten times
- *
- */
-
-void print_alphabet_x10()
-{
-	int i = 0;
-
-	while (i < 10)
-	{
-		print_alphabet();
-		i++;
-	}
-	_putchar('\n');
-}
->>>>>>> f8bebc06d67ab23e894610690ab8ba648e90b734
