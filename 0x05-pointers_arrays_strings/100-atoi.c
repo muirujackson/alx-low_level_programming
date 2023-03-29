f171675d1628bfdf87d9b5a949a75243a231e625
muirujackson <jakezweru@gmail.com>
2023-03-29 00:20:20 -0700

    negative sign

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,21 +8,25 @@
 
 int _atoi(char *s)
 {
-    int n = 0;
+    int n = 0, neg = 1;
     char isNumber = 'F';
+
 
     while (*s != '\0')
     {
-            if (*s >= '0' && *s <= '9')
-            {
-                    n = (n * 10) + *s;
-                    s++;
-                    isNumber = 'T';
-            } else {
-                    if (isNumber == 'T')
-                            break;
-                    s++;
-            }
+
+        if(*s == '-')
+            neg = -1;
+        if (*s >= '0' && *s <= '9')
+        {
+            n = (n * 10) + *s;
+            s++;
+            isNumber = 'T';
+        } else {
+            if (isNumber == 'T')
+                break;
+            s++;
+        }
     }
-    return (n);
+    return (n * neg);
 }
