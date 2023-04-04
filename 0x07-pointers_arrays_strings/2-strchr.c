a55cbe7f9e855ed0ee29f8d978c6bdcc51470bf2
jakezweru <jakezweru@gmail.com>
2023-04-04 01:38:36 -0400

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,13 +15,6 @@ char *_strchr(char *s, char c)
 			return (s);
 		s++;
 	}
-	/**
-	 * if c is '\0', you should return
-	 * the pointer to the '\0' of the
-	 * string s
-	*/
-	if (*s == c)
-		return (s);
-	/*return null if not found*/
-	return ('\0');
+
+	return (NULL);
 }
