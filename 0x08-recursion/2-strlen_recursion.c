06b9e0e08ec90c242894b2c19851ec78862a5799
muirujackson <jakezweru@gmail.com>
2023-04-06 10:21:52 -0700

    betty style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,15 +4,15 @@
  * _strlen_recursion - count string length
  * @s: string to count length
  *
- * return: void
+ * return: int value
  */
 int _strlen_recursion(char *s)
 {
 	int i;
 
 	if (*s == '\0')
-		return 1;
-	i = 0;
+		return (0);
+	i = 1;
 	s = (s + 1);
 	return (i + _strlen_recursion(s));
 }
