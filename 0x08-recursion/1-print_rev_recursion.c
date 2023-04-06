098ad74437a59692f8a42a80df0361bce937e94d
muirujackson <jakezweru@gmail.com>
2023-04-06 07:17:08 -0700

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints string
+ * @s: parameter of the function
+ *
+ * return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s + 1);
+	_putchar(*s);
+}
