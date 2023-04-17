9194c26ed348d4b1b3b9b0142fd04fa14af992e0
muirujackson <jakezweru@gmail.com>
2023-04-17 13:28:15 -0700

    iniliazation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+typedef struct dog dog_t;
 /**
  * struct dog - poppy structure
  * @name: name of the dog
@@ -15,7 +16,6 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
