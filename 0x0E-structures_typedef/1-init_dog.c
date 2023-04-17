4cfdff9eea8585874fb5c44e28c07f64625fdc41
muirujackson <jakezweru@gmail.com>
2023-04-17 14:21:47 -0700

    initialization function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,5 @@
+#include "dog.h"
+
 /**
  * init_dog - initialize dog struct
  *
@@ -12,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
