19f8e54511c35c7050c7513dcbd5b9803841087a
muirujackson <jakezweru@gmail.com>
2023-04-17 11:50:28 -0700

    betty style

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,16 @@
 /**
  * init_dog - function to initialize dog variables
  * @d: where we store dog record
- * @name: the name of the dog 
+ * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the ownwer of the dog
- * 
- * Return: nothing 
+ *
+ * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = *name;
-	d -> age = age;
-	d -> owner = *owner;
-}
+	d->name = *name;
+	d->age = age;
+	d->owner = *owner;
+};
 
