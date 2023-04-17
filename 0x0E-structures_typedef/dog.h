1b8c66ac54e6c24f4884457eabd61b1805bcbc72
muirujackson <jakezweru@gmail.com>
2023-04-17 11:00:31 -0700

    betty style added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - poppy structure
  * @name: name of the dog
- * @age: age of the dog 
+ * @age: age of the dog
  * @owner: the owner of the dog
  *
  * Description: Declare record of a dog
  */
 struct dog
 {
-	char * name;
-	char * owner;
+	char *name;
+	char *owner;
 	float age;
-}	
+}
+
+#endif
