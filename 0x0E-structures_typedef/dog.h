daf107a560dbb5b12f4635f8938bfc999e6f6733
muirujackson <jakezweru@gmail.com>
2023-04-17 14:06:23 -0700

    moved files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - poppy structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: Declare record of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
