07cb03a5ecf1aee5c3b0dcfeb2b78335500d22a2
muirujackson <jakezweru@gmail.com>
2023-04-17 18:52:19 -0700

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Program compilation starts from the main
+ * Description: prints the name
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
