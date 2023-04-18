a05cbb124431a3fa5f91085602baa6c27edf5cc9
muirujackson <jakezweru@gmail.com>
2023-04-17 18:22:47 -0700

    added conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,18 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %s\n", d->age);
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+
+		else
 		printf("Owner: %s\n", d->owner);
 	}
 }
