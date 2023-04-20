b7f972d3b691a5103594e8270483cca0665d6bf3
muirujackson <jakezweru@gmail.com>
2023-04-19 18:58:19 -0700

    final ans

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,35 +9,37 @@
  * @argv: Arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
-	int firstNum, secondNum;
+	int num1, num2;
 	char *operator;
 
-	operator = argv[2];
-	secondNum = atoi(argv[3]);
-	firstNum = atoi(argv[1]);
-	if (argc == 4)
+	/* Error if arguments are not 4 with the executable */
+	if (argc != 4)
 	{
-		int (*p)(int, int);
-
-		p = get_op_func(operator);
-		if (p == NULL || operator[1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		if ((*operator == '/' && !secondNum) || (*operator == '%' && !secondNum))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-
-
-		printf("%d\n", get_op_func(operator)(firstNum, secondNum));
-		return (0);
+		printf("Error\n");
+		exit(98);
 	}
-	printf("Error\n");
-	exit(98);
+
+	num1 = atoi(argv[1]); /*if argument is string convert to number*/
+	num2 = atoi(argv[3]); /* as stated above */
+	operator = argv[2];
+
+	/* Error if operator is not null or a 2d array '\*' */
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	/* Error if user tries to divide / (47) or % (37) by 0*/
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2));
+
+	return (0);
 }
