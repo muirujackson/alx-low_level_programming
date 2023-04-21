bf5468ebc169b8f7d116e48dfc447f4ea5a73982
muirujackson <jakezweru@gmail.com>
2023-04-21 15:08:04 -0700

    3 print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,5 @@
+#include <stdarg.h>
+#include <stdio.h>
 #ifndef VAR_H
 #define VAR_H
 
@@ -6,5 +8,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+/**
+ * struct print - A new struct type defining our print
+ *
+ * @symbol: Our symbol representing the data type
+ * @print_func: a function pointer that prints
+ *             a data type according to the symbol
+*/
+typedef struct print
+{
+	char *symbol;
+	void (*print_func)(va_list arg);
+} func_printer;
 
 #endif
