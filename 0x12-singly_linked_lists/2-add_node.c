1cf0e1688144e42e0df8d435b59c2ca34cf4455f
muirujackson <jakezweru@gmail.com>
2023-04-27 18:46:49 -0700

    added betty style to quiz 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 
 /**
  * add_node - function to add node
@@ -9,7 +9,7 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t new_node;
+	list_t *new_node;
 	int string_len;
 
 	string_len = 0;
@@ -17,8 +17,8 @@ list_t *add_node(list_t **head, const char *str)
 
 	while (str[string_len] != '\0')
 		string_len++;
-	new_node->str = strdup(*str);
-	new_node->len = (string_len - 1);
+	new_node->str = strdup(str);
+	new_node->len = (string_len);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
