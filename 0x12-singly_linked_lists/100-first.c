fcd74f77d95b1545d11d0ba00692bd0fc1fe3c91
muirujackson <jakezweru@gmail.com>
2023-04-27 19:52:36 -0700

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include  "lists.h"
+/**
+ * first_main - function executed before main
+ * Return: no return.
+ */
+void first_main() __attribute__((constructor))
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+void start_main() __attribute__((constructor));
 size_t list_len(const list_t *h);
 int _putchar(char c);
 size_t print_list(const list_t *h);
