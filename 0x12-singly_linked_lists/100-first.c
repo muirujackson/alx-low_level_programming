327241496ba24cbce550fd90230098df146178b0
muirujackson <jakezweru@gmail.com>
2023-04-27 19:56:34 -0700

    added betty style to quiz 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * first_main - function executed before main
  * Return: no return.
  */
-void first_main() __attribute__((constructor))
+void first_main(void) __attribute__((constructor))
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
