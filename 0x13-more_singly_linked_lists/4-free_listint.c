e5d45e6b3f89043e483f15dfaf610cb59308b341
muirujackson <jakezweru@gmail.com>
2023-05-01 11:38:03 -0700

    quiz 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - function to free the memory
+ * @head: pointer to head
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t tmp;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
