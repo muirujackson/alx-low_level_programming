eae6fdf512967c004312b5bfc6cc17e50d3dc2ab
muirujackson <jakezweru@gmail.com>
2023-05-01 04:52:45 -0700

    quiz 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - count number of element
+ * @h: pointer to head
+ *
+ * Return: lenght of list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t length;
+
+	length = 0;
+	while (h != NULL)
+	{
+		length++;
+		h = h->next;
+	}
+}	
