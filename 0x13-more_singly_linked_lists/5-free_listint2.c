899a8d944f2e238c21569893ff046eda5ceb8214
muirujackson <jakezweru@gmail.com>
2023-05-02 06:21:23 -0700

    added return

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL)
-		return;
 	while ((tmp = *head) != NULL)
 	{
 		*head = (*head)->next;
 		free(tmp);
 	}
 	*head = NULL;
+	return;
 }
