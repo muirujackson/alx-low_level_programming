b49f83cbe6931edd47b747bdb3f5581a3acc2b3b
muirujackson <jakezweru@gmail.com>
2023-05-02 05:28:17 -0700

    quiz 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees nodes
+ * @head: a pointer to head
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
