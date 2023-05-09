bc6561b39dfbb7d9fa2d784c26dfee7f28206aae
muirujackson <jakezweru@gmail.com>
2023-05-08 18:14:21 -0700

    betty added

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,18 +13,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer = malloc(sizeof(char) * letters);
 	ssize_t read_bytes;
 
-	if (filename == NULL)
-		return 0;
+	if (filename == NULL || buffer == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return 0;
+		return (0);
 	read_bytes = read(fd, buffer, letters);
 	if (read_bytes != -1)
 		dprintf(STDOUT_FILENO, "%s", buffer);
 	else
 	{
 		close(fd);
-		return 0;
+		return (0);
 	}
 	free(buffer);
 	close(fd);
