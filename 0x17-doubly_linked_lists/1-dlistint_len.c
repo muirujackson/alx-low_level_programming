e0ef16addf85be2d032465b7712bca73197ed528
muirujackson <jakezweru@gmail.com>
2023-06-15 10:14:39 -0700

    double linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return len
+ * *h: pointer to header
+ *
+ * Return: size_t
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t length = 0;
+
+	while (h != NULL)
+	{
+		length++;
+		h = h->next;
+	}
+	return (length);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+learning doublely linked list in c
