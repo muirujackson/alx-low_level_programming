0f5e423d08e9ef0615a9d22d087eae1ccafa88d0
muirujackson <jakezweru@gmail.com>
2023-07-07 04:11:55 -0700

    insert method

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
+	hash_node_t *tmp;
 
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
@@ -20,7 +21,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->key = key;
 	new->value = strdup(value);
 	new->next = NULL;
-	ht->array[key_index(key, ht->size)] = new;
+
+
+	if (!ht->array[key_index(key, ht->size)])
+		ht->array[key_index(key, ht->size)] = new;
+	else
+	{
+		tmp = ht->array[key_index(key, ht->size)];
+		while (tmp->next)
+		{
+			tmp = tmp->next;
+		}
+
+		tmp->next = new;
+	}
 
 	return 1;
 }
