b2f802ab00c2aafe1783afe37f030c9ac8f6f474
muirujackson <jakezweru@gmail.com>
2023-07-07 16:08:26 -0700

    betty style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,12 +13,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *current;
 	unsigned long int index;
 
-	if (!ht || !key || *key == '\0' )
+	if (!ht || !key || *key == '\0')
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	/*  Check if the key already exists */
 	current = ht->array[index];
-	
+
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			/* Key already exists, update the value */
 			free(current->value);
 			current->value = strdup(value);
-			return 1;
+			return (1);
 		}
 		current = current->next;
 	}
