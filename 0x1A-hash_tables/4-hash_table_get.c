cb7dba7fa5fdaa96dc905428261a652286fec47f
muirujackson <jakezweru@gmail.com>
2023-07-07 13:27:20 -0700

    get retrieve

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,3 +1,28 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get: 
+ * @ht: hash table
+ * @key: the key to retrieve value of
+ *
+ * Return: a string
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t * tmp;
+
+	index = key_index((const unsigned char *) key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
 
