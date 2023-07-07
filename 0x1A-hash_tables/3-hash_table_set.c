95ffed4d894ece880ea7ad6aacce4b25d8acb28f
muirujackson <jakezweru@gmail.com>
2023-07-07 09:23:45 -0700

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *current;
 	unsigned long int index;
 
+	if (!ht || !key || *key == '\0' )
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	/*  Check if the key already exists */
 	current = ht->array[index];
@@ -32,8 +34,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
-		return (0);
-	if (!ht || !key || *key == '\0' )
 		return (0);
 	new->key = strdup(key);
 	new->value = strdup(value);
