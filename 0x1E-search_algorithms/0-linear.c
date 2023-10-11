9627ab748306bfce86658f99c158152b39709586
muirujackson <jakezweru@gmail.com>
2023-10-11 14:57:08 -0700

    added betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,17 +9,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-        int i = 0;
+	int i = 0;
 
-        if (array == NULL)
-                return -1;
-        while (size > 0)
-        {
-                printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	if (array == NULL)
+		return (-1);
+	while (size > 0)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-                        return i;
-                i++;
-                size--;
-        }
-	return -1;
+			return (i);
+		i++;
+		size--;
+	}
+	return (-1);
 }
